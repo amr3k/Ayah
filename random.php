2626570192941c06373ff85d80db550361153e08
akkk33 <akkk33@users.noreply.github.com>
2016-12-28 23:03:19 +0200

    Add files via upload

---
+++ random.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+require ('configuration.php');
+$SQLCommand = 	'SELECT imlai FROM Quran ORDER BY RAND() LIMIT 1';
+$result		=	mysqli_query($db , $SQLCommand);
+		while($row = mysqli_fetch_array($result , MYSQLI_ASSOC)){
+			echo $row['imlai'];
+		}
