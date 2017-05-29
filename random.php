b5fa77041075c6e50575ea1e008d8a59ff116803
akkk33 <akkk33@protonmail.com>
2017-05-30 01:02:10 +0200

    deleted old files

--- random.php
+++
@@ -1,7 +0,0 @@
-<?php
-require ('configuration.php');
-$SQLCommand = 	'SELECT imlai FROM Quran ORDER BY RAND() LIMIT 1';
-$result		=	mysqli_query($db , $SQLCommand);
-		while($row = mysqli_fetch_array($result , MYSQLI_ASSOC)){
-			echo $row['imlai'];
-		}
